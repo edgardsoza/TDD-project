abd11a31b01b0700d52d81528535e4a8559fc3ac
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-01 09:47:35 -0600

    Updated fizzbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -19,20 +19,20 @@ describe Solver do
 
   context 'Testing the solver.fizzbuzz method' do
     it 'returns "fizz" when the number is divisible by 3' do
-      expect(fizzbuzz(3)).to eq 'fizz'
-      expect(fizzbuzz(36)).to eq 'fizz'
+      expect(solver.fizzbuzz(3)).to eq 'fizz'
+      expect(solver.fizzbuzz(36)).to eq 'fizz'
     end
     it 'returns "buzz" when the number is divisible by 5' do
-      expect(fizzbuzz(10)).to eq 'buzz'
-      expect(fizzbuzz(50)).to eq 'buzz'
+      expect(solver.fizzbuzz(10)).to eq 'buzz'
+      expect(solver.fizzbuzz(50)).to eq 'buzz'
     end
     it 'returns "fizzbuzz" when the number is divisible by 3 and 5' do
-      expect(fizzbuzz(15)).to eq 'fizzbuzz'
-      expect(fizzbuzz(45)).to eq 'fizzbuzz'
+      expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      expect(solver.fizzbuzz(45)).to eq 'fizzbuzz'
     end
     it 'returns the number as a string for any other case' do
-      expect(fizzbuzz(7)).to eq '7'
-      expect(fizzbuzz(14)).to eq '14'
+      expect(solver.fizzbuzz(7)).to eq '7'
+      expect(solver.fizzbuzz(14)).to eq '14'
     end
   end
   context "When testing the reverse method" do
